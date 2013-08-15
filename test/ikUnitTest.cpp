bce3048646234c68bddea1358d9c272d15d6420c
Michael Grey <mxgrey@gatech.edu>
2013-08-14 23:31:53 -0400

    Major fixes/changes to DLS IK

--- test/ikUnitTest.cpp
+++ test/ikUnitTest.cpp
@@ -50,14 +50,12 @@ void ikTest()
     cout << "--------------------------------------------" << endl;
 
 
-    Robot parseTest("../urdf/drchubo.urdf");
+    Robot parseTest("../urdf/drchubo_v2.urdf");
     parseTest.linkage("Body_RSP").name("RightArm");
     parseTest.linkage("Body_LSP").name("LeftArm");
     parseTest.linkage("Body_RHY").name("RightLeg");
     parseTest.linkage("Body_LHY").name("LeftLeg");
 
-
-    bool totallyRandom = true;
 
     string limb = "LeftArm";
 
@@ -90,24 +88,40 @@ void ikTest()
         restValues[5] = 0;
         restValues[6] = 0;
     }
-
+    
+    
+    bool totallyRandom = false;
+    int resolution = 1000;
+    double scatterScale = 0.045;
+    
+    int tests = 10000;
 
 
     Constraints constraints;
     constraints.restingValues(restValues);
     constraints.performNullSpaceTask = false;
+    constraints.maxAttempts = 1;
+    constraints.maxIterations = 200;
+    constraints.convergenceTolerance = 0.001;
+//    constraints.wrapToJointLimits = true;
+//    constraints.wrapSolutionToJointLimits = true;
+    constraints.wrapToJointLimits = false;
+    constraints.wrapSolutionToJointLimits = false;
 
     TRANSFORM target;
 
-    int wins = 0;
+    int wins = 0, jumps = 0;
 
     srand(time(NULL));
-
-    int tests = 1000;
+    
+    clock_t time;
+    time = clock();
+    
+    
+    
+    
     for(int k=0; k<tests; k++)
     {
-        int resolution = 1000;
-        double scatterScale = 0.1;
         for(int i=0; i<parseTest.linkage(limb).nJoints(); i++)
         {
             int randomVal = rand();
@@ -128,11 +142,11 @@ void ikTest()
                         + parseTest.linkage(limb).joint(i).min();
             ////////////////////// NOT COMPLETELY RANDOM TEST //////////////////////
             else
-                jointValues(i) = targetValues(i) +
-                        ((double)(2*rand()%resolution)/((double)resolution-1)-1)*scatterScale
-                        *(parseTest.linkage(limb).joint(i).max() - parseTest.linkage(limb).joint(i).min());
 //                jointValues(i) = targetValues(i) +
-//                        ((double)(2*rand()%resolution)/((double)resolution-1)-1)*scatterScale;
+//                        ((double)(2*rand()%resolution)/((double)resolution-1)-1)*scatterScale
+//                        *(parseTest.linkage(limb).joint(i).max() - parseTest.linkage(limb).joint(i).min());
+                jointValues(i) = targetValues(i) +
+                        ((double)(2*rand()%resolution)/((double)resolution-1)-1)*scatterScale;
 
             parseTest.linkage(limb).joint(i).value(targetValues(i));
         }
@@ -152,6 +166,35 @@ void ikTest()
         rk_result_t result = parseTest.dampedLeastSquaresIK_linkage(limb, jointValues, target, constraints);
         if( result == RK_SOLVED )
             wins++;
+        
+        
+//        if(false)
+        if(!totallyRandom && (result != RK_SOLVED || (storedVals-jointValues).norm() > 1.1*(storedVals-targetValues).norm()) )
+        {
+            if( result == RK_SOLVED )
+                cout << "SOLVED" << endl;
+            else
+            {
+                Vector3d Terr = target.translation() - parseTest.linkage(limb).tool().respectToRobot().translation();
+                AngleAxisd aaerr;
+                aaerr = target.rotation()*parseTest.linkage(limb).tool().respectToRobot().rotation().transpose();
+                Vector3d Rerr;
+                if(fabs(aaerr.angle()) <= M_PI)
+                    Rerr = aaerr.angle()*aaerr.axis();
+                else
+                    Rerr = (aaerr.angle()-2*M_PI)*aaerr.axis();
+                
+                cout << "FAILED (" << Terr.norm() << ", " << Rerr.norm() << ")" << endl;
+            }
+            cout << "Start: " << storedVals.transpose() << endl;
+            cout << "Solve: " << jointValues.transpose() << endl;
+            cout << "Goal : " << targetValues.transpose() << endl;
+            cout << "Delta: " << (storedVals-targetValues).transpose() << "\t(" << (storedVals-targetValues).norm() << ")" << endl;
+            cout << "Diff : " << (storedVals-jointValues).transpose() << "\t(" << (storedVals-jointValues).norm() << ")" << endl << endl;
+            if( result == RK_SOLVED )
+                jumps++;
+        }
+            
 
 //        cout << "End:" << endl;
 //        cout << parseTest.linkage(limb).tool().respectToRobot().matrix() << endl;
@@ -184,10 +227,18 @@ void ikTest()
                                   - target.translation()).norm() << endl;
         }
     }
+    
+    
+    clock_t endTime;
+    endTime = clock();
+    cout << "Time: " << (endTime - time)/((double)CLOCKS_PER_SEC*tests) << " : " <<
+            (double)CLOCKS_PER_SEC*tests/(endTime-time) << endl;
+    
 
     cout << "Win: " << ((double)wins)/((double)tests)*100 << "%" << endl;
 
-
+    if(!totallyRandom)
+        cout << "Jump: " << ((double)jumps)/((double)tests)*100 << "%" << endl;
 
 
 
